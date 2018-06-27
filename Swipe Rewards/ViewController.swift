2380dc1776f41de8d6b1ba0833efcabbf62d9e11
Bharath <Bharthanb@winjit.com>
2018-06-27 17:57:41 +0530

    Initial Commit

---
+++ Swipe Rewards/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swipe Rewards
+//
+//  Created by Bharathan on 27/06/18.
+//  Copyright © 2018 SwipeRewards. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
