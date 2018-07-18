a5f61645f4617a6fd5a4bf8389189ff98199d1b5
Bharath <Bharthanb@winjit.com>
2018-07-18 12:48:55 +0530

    initial commit

---
+++ Swipe Rewards/Helpers/SwipeRewardsAPI.swift
@@ -0,0 +1,14 @@
+//
+//  SwipeRewardsAPI.swift
+//  Swipe Rewards
+//
+//  Created by Bharathan on 18/07/18.
+//  Copyright © 2018 SwipeRewards. All rights reserved.
+
+
+//Swipe Rewards Server Request API
+import UIKit
+class SwipeRewardsAPI: NSObject {
+static let sharedInstance = SwipeRewardsAPI()
+    static let serverURL = "https://"
+}
